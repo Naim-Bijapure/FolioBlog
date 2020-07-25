359ec4ac202f766ae8181dc163def1d54538c783
NaimBijapure <naim@vinnovatetechnoligies.com>
2020-07-25 12:48:16 +0530

    updated about page typer animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,73 +8,9 @@ import { Paper, Slide, Zoom } from "@material-ui/core"
 
 import { device } from "../styles/device"
 import { PRIMARY } from "../utils/constants"
+import Typer from "./Typer"
 
 let Description = ""
-
-const Wrapper = styled.div`
-  /* border: 1px solid red; */
-  height: 155px;
-  max-width: 100%;
-  padding: 1%;
-
-  .Paper {
-    /* border: 1px solid red; */
-    height: 100px;
-    margin: 0.5rem;
-    border-top: 3px solid ${props => props.theme.PRIMARY};
-  }
-
-  @media ${device.laptop} {
-    /* border: 1px solid red; */
-  }
-`
-const Flex = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  bottom: 9.5rem;
-`
-const ProfileImage = styled.div`
-  width: 12rem;
-  border-radius: 100%;
-  /* margin-left: 3rem;
-  margin-top: 3rem; */
-  position: relative;
-  top: 1rem;
-  right: 1rem;
-
-  img {
-    border-radius: 100%;
-  }
-  .Profile__Paper {
-    border-radius: 100%;
-  }
-`
-
-Description = styled.div`
-  /* border: 1px solid blue; */
-  position: relative;
-  bottom: 2rem;
-  font-size: 1.5rem;
-  line-height: 2.4rem;
-  font-weight: 900;
-  top: 1rem;
-
-  @media ${device.tablet} {
-    /* border: 1px solid red; */
-    width: 50%;
-    font-size: 1.7rem;
-    line-height: 2.5rem;
-    position: relative;
-    left: 2rem;
-
-    & ${Description}:nth-child(2) {
-      position: relative;
-      left: 4rem;
-    }
-  }
-`
 
 export default function Header() {
   const data = useStaticQuery(
@@ -124,7 +60,7 @@ export default function Header() {
           >
             <defs>
               <filter id="p1" x="0" y="0">
-                <feGaussianBlur in="SourceGraphic" stdDeviation="3" />
+                <feGaussianBlur in="SourceGraphic" stdDeviation="1" />
               </filter>
             </defs>
 
@@ -136,12 +72,12 @@ export default function Header() {
               filter="url(#p1)"
             ></path>
 
-            <path
+            {/* <path
               d="M-0.94,-68.58 C13.16,348.84 452.22,-16.28 532.35,224.50 L482.69,179.10 L0.00,150.00 Z"
               // d="M-0.94,-68.58 C335.96,-113.98 452.22,-16.8 536.31,-75.49 L446.57,100.16 L86.53,102.14 Z "
               style={{ stroke: "none", fill: PRIMARY }}
               filter="url(#p1)"
-            ></path>
+            ></path> */}
           </svg>
 
           <Flex>
@@ -158,8 +94,14 @@ export default function Header() {
 
             <Zoom in={isZoomDescription} timeout={1000}>
               <Description>
-                <div>Naim Bijapure</div>
-                <div> Full Stack Developer</div>
+                <div className="Name">Naim Bijapure,</div>
+                {/* <div> Full Stack Web Developer</div> */}
+                <Typer
+                  titleText={[
+                    "Full Stack Web Developer",
+                    "I build things for web...",
+                  ]}
+                />
               </Description>
             </Zoom>
           </Flex>
@@ -168,3 +110,98 @@ export default function Header() {
     </Wrapper>
   )
 }
+
+// STYLES
+const Wrapper = styled.div`
+  /* border: 1px solid red; */
+  height: 155px;
+  max-width: 100%;
+  padding: 1%;
+
+  .Paper {
+    /* border: 1px solid red; */
+    height: 100px;
+    margin: 0.5rem;
+    border-top: 3px solid ${props => props.theme.PRIMARY};
+  }
+
+  @media ${device.laptop} {
+    /* border: 1px solid red; */
+  }
+`
+const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  bottom: 9.5rem;
+  /* border:1px solid red; */
+`
+const ProfileImage = styled.div`
+  width: 12rem;
+  border-radius: 100%;
+  /* margin-left: 3rem;
+  margin-top: 3rem; */
+  position: relative;
+  top: 1rem;
+  /* right: 1rem; */
+  right: 1rem;
+  border: 1px solid ${props => props.theme.PRIMARY};
+  box-shadow: 0px 0px 9px 1px ${props => props.theme.PRIMARY};
+
+  img {
+    border-radius: 100%;
+  }
+  .Profile__Paper {
+    border-radius: 100%;
+  }
+`
+
+Description = styled.div`
+  /* border: 1px solid blue; */
+  width: 20rem;
+  position: relative;
+  bottom: 1rem;
+  font-size: 1.3rem;
+  line-height: 2rem;
+  font-weight: 900;
+  top: 1rem;
+  color: ${props => props.theme.PRIMARY};
+  .Name {
+    position: relative;
+    width: 11.2rem;
+    /* border:1px solid red; */
+    background: ${props => props.theme.PRIMARY};
+    color: ${props => props.theme.SECONDARY};
+    border-radius: 4px;
+    text-align: center;
+  }
+  .Typer {
+    background: ${props => props.theme.PRIMARY};
+    color: ${props => props.theme.SECONDARY};
+    border-radius: 4px;
+    text-align: center;
+  }
+
+  /*  ABOUT TABLE SCREENS */
+  @media ${device.tablet} {
+    /* border: 1px solid red; */
+    width: 50%;
+    font-size: 1.7rem;
+    line-height: 2.5rem;
+    position: relative;
+    left: 2rem;
+
+    & ${Description}:nth-child(2) {
+      position: relative;
+      left: 4rem;
+    }
+    .Name {
+      width: 21.2rem;
+    }
+    .Typer {
+      text-align: center;
+      width:30rem;
+    }
+  }
+`
---
+++ src/components/Typer.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect, useRef } from "react"
+import styled from "styled-components"
+
+export default function Typer({ titleText }) {
+  const [state, setState] = useState({
+    text: "",
+    isDeleting: false,
+    loopNum: 0,
+    typingSpeed: 1500,
+  })
+
+  useEffect(() => {
+    // setTimeout(() => {
+    // }, 2000)
+    handleType()
+  }, [])
+
+  function handleType() {
+    setState(state => {
+      // // console.log("handleType -> state", state)
+      let dataText = [...titleText]
+      const { isDeleting, loopNum, text, typingSpeed } = state
+      const i = loopNum % dataText.length
+      const fullText = dataText[i]
+
+      setState(state => ({
+        ...state,
+        text: state.isDeleting
+          ? fullText.substring(0, text.length - 1)
+          : fullText.substring(0, text.length + 1),
+        typingSpeed: state.isDeleting ? 300 : 550,
+      }))
+
+      if (!isDeleting && text === fullText) {
+        setTimeout(
+          () => setState(state => ({ ...state, isDeleting: true })),
+          1500
+        )
+      } else if (isDeleting && text === "") {
+        setState(state => ({
+          ...state,
+          isDeleting: false,
+          loopNum: state.loopNum + 1,
+        }))
+      }
+
+      setTimeout(handleType, typingSpeed)
+      return state
+    })
+  }
+
+  return (
+    <Container className="Typer">
+      <div>
+        {state.text} <span id="cursor"></span>
+      </div>
+      {/* <button onClick={handleType}>click</button> */}
+    </Container>
+  )
+}
+
+// Styles
+const Container = styled.div`
+  position: relative;
+  /* border:1px solid red; */
+  #cursor {
+    border-left: 0.1em solid white;
+    animation: blink 0.7s steps(1) infinite;
+  }
+
+  @keyframes blink {
+    50% {
+      border-color: ${props => props.theme.PRIMARY};
+    }
+  }
+`
